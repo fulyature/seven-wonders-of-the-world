3e2965671d986f735580bb6a50d4dc1ee79adf9c
fulya_ture <fulyature@gmail.com>
2023-07-16 02:19:47 +0300

    useState

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <h2 className="text-center">NEW SEVEN WONDERS OF THE WORLD</h2>
+      <h2 className="text-center display-5 m-5">
+        NEW SEVEN WONDERS OF THE WORLD
+      </h2>
     </div>
   );
 };
--- src/components/Wonders.jsx
+++ src/components/Wonders.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { Card, Row } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import data from "../helper/data";
+import { arrowdown } from "../helper/icons";
+import { arrowup } from "../helper/icons";
+import { useState } from "react";
 
 const Wonders = () => {
-  return <div>Wonders</div>;
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <Container>
+      {data.map((items) => {
+        const { wonder, information, id } = items;
+        return (
+          <Card key={id}>
+            <Card.Body>
+              <h5>{wonder}</h5>
+              {toggle ? (
+                <button>{arrowdown} </button>
+              ) : (
+                <button>{arrowup} </button>
+              )}
+            </Card.Body>
+          </Card>
+        );
+      })}
+    </Container>
+  );
 };
 
 export default Wonders;
