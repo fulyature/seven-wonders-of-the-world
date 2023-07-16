7c1a8661c9cb5324dece8d09fb6f9a02f16de573
fulya_ture <fulyature@gmail.com>
2023-07-16 03:02:46 +0300

    fixed issues

--- src/components/Wonders.jsx
+++ src/components/Wonders.jsx
@@ -1,4 +1,4 @@
-import { Card, Row } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import data from "../helper/data";
 import { arrowdown } from "../helper/icons";
@@ -6,21 +6,27 @@ import { arrowup } from "../helper/icons";
 import { useState } from "react";
 
 const Wonders = () => {
-  const [toggle, setToggle] = useState(false);
+  const [activeId, setActiveId] = useState(null);
 
   return (
     <Container>
       {data.map((items) => {
-        const { wonder, information, id } = items;
+        const { wonder, information, id, src } = items;
         return (
           <Card key={id}>
-            <Card.Body>
-              <h5>{wonder}</h5>
-              {toggle ? (
-                <button>{arrowdown} </button>
-              ) : (
-                <button>{arrowup} </button>
-              )}
+            <Card.Body className="d-flex justify-content-between card-body">
+              <img className="w-50 p-3" src={src} alt="" />
+              <div className="container text-justify text-center">
+                <h5 className="">{wonder}</h5>
+                {activeId === id && <div>{information}</div>}
+              </div>
+              <button
+                onClick={() =>
+                  activeId === id ? setActiveId(null) : setActiveId(id)
+                }
+              >
+                {activeId === id ? arrowup : arrowdown}
+              </button>
             </Card.Body>
           </Card>
         );
